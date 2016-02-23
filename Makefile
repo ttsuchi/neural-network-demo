f396b06fac944ae466f42d530834e641444f2fb9
ersp-admin <ersp-admin@ersp1.com>
2016-02-22 19:57:52 -0500

    Pushing before attempting Makefile git pushing

---
+++ Makefile
@@ -0,0 +1,7 @@
+# Makefile for pushing our files to git
+
+git:
+	rm -f *~ .*.*.swo .*.*.swp
+	git add .
+	git commit -m "No commit message"
+	git push
---
+++ train2.py
@@ -0,0 +1,25 @@
+from kivy.uix.scrollview import ScrollView
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.app import App
+from kivy.uix.slider import Slider
+from kivy.uix.image import Image
+from kivy.uix.dropdown import DropDown
+from kivy.uix.textinput import TextInput
+
+class runTrain(App):
+
+    def build(self):
+        layout = FloatLayout()
+
+        return layout
+
+
+class selectTrainOption(FloatLayout):
+
+    def __init__(self, **kwargs):
+        #super(selectTrainOption, self).__init__(**kwargs)
+        
+
+
