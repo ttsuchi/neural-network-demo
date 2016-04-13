4da85a241c650a39d0d812abde27e2b12ba7cc7a
ersp-admin <ersp-admin@ersp1.com>
2016-04-13 00:57:39 -0400

    Updated gitignore

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ from kivy.uix.scrollview import ScrollView
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
+from kivy.uix.togglebutton import ToggleButton
 from kivy.app import App
 from kivy.uix.slider import Slider
 from kivy.uix.image import Image
@@ -20,9 +21,9 @@ class Train(Screen):
         layout = FloatLayout()
         
         # Buttons for layout
-        layout.add_widget(Button(text='Train for gender', size_hint=(.2, .1), pos_hint={'x': .1, 'center_y': .83}))
-        layout.add_widget(Button(text='Train for expression', size_hint=(.2, .1), pos_hint={'x': .4, 'center_y': .83}))
-        layout.add_widget(Button(text='Train for identity', size_hint=(.2, .1), pos_hint={'center_x': .8, 'center_y': .83}))
+        layout.add_widget(ToggleButton(text='Train for gender', group='training', size_hint=(.2, .1), pos_hint={'x': .1, 'center_y': .83}))
+        layout.add_widget(ToggleButton(text='Train for expression', group='training', size_hint=(.2, .1), pos_hint={'x': .4, 'center_y': .83}))
+        layout.add_widget(ToggleButton(text='Train for identity', group='training', size_hint=(.2, .1), pos_hint={'center_x': .8, 'center_y': .83}))
         layout.add_widget(Button(text='Previous'
             , size_hint=(.2, .1), pos_hint={'x': .15, 'center_y': .15}))
         loadButton = Button(text='Train', size_hint=(.2, .1), pos_hint={'x': .6, 'center_y': .15})
@@ -34,16 +35,16 @@ class Train(Screen):
 
         #Labels and Input Text Boxes
         layout.add_widget(Label(text="# of hidden units", size_hint=(.2, .1), pos_hint={'x': .1, 'center_y': .65}))
-        layout.add_widget(TextInput(text='0.1', size_hint=(.07, .05), pos_hint={'x': .3, 'center_y': .65}))
+        layout.add_widget(TextInput(text='10', size_hint=(.07, .05), pos_hint={'x': .3, 'center_y': .65}))
 
 
         layout.add_widget(Label(text="# of validations", size_hint=(.2, .1), pos_hint={'x': .1, 'center_y': .55}))
-        layout.add_widget(TextInput(text='0.1', size_hint=(.07, .05), pos_hint={'x': .3, 'center_y': .55}))
+        layout.add_widget(TextInput(text='0', size_hint=(.07, .05), pos_hint={'x': .3, 'center_y': .55}))
 
         layout.add_widget(Label(text="# of test data", size_hint=(.2, .1), pos_hint={'x': .1, 'center_y': .45}))
-        layout.add_widget(TextInput(text='0.1', size_hint=(.07, .05), pos_hint={'x': .3, 'center_y': .45}))
+        layout.add_widget(TextInput(text='0', size_hint=(.07, .05), pos_hint={'x': .3, 'center_y': .45}))
         
-        layout.add_widget(Label(text="# of hidden units \n    learning rate", size_hint=(.2, .1), pos_hint={'x': .1, 'center_y': .35}))
+        layout.add_widget(Label(text="hidden units \n    learning rate", size_hint=(.2, .1), pos_hint={'x': .1, 'center_y': .35}))
         layout.add_widget(TextInput(text='0.1', size_hint=(.07, .05), pos_hint={'x': .3, 'center_y': .35}))
 
 
@@ -54,7 +55,7 @@ class Train(Screen):
         layout.add_widget(TextInput(text='0.2', size_hint=(.07, .05), pos_hint={'x': .8, 'center_y': .55}))
 
         layout.add_widget(Label(text="# of epochs needed \n  to train", size_hint=(.2, .1), pos_hint={'x': .6, 'center_y': .45}))
-        layout.add_widget(TextInput(text='0.2', size_hint=(.07, .05), pos_hint={'x': .8, 'center_y': .45}))
+        layout.add_widget(TextInput(text='0', size_hint=(.07, .05), pos_hint={'x': .8, 'center_y': .45}))
 
         layout.add_widget(Label(text="or minimum RMSE", size_hint=(.2, .1), pos_hint={'x': .6, 'center_y': .35}))
         layout.add_widget(TextInput(text='0.2', size_hint=(.07, .05), pos_hint={'x': .8, 'center_y': .35}))
