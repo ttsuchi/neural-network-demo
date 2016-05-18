336255132c301ac6e87cf8e49f3bcbdb613b00aa
ersp-admin <ersp-admin@ersp1.com>
2016-05-18 00:04:00 -0400

    Back button to result.py

--- result.py
+++ result.py
@@ -17,21 +17,23 @@ class Example(Screen):
                 row_default_height=75, size_hint_y=None)
         # Make sure the height is such that there is something to scroll.
         label_layout = FloatLayout()
+
+
+        # back button
+
+        backButton = Button(text='Back', size_hint=(.15, .1), pos=(60,500))
+        label_layout.add_widget(backButton)
+
+        backButton.bind(on_press=self.goBack)
+
+
         graph = Graph(xLabel='X',ylabel='Y',x_ticks_minor=5, \
-                        x_ticks_major=25,y_ticks_major=1,y_grid_label=True, \
-                        x_grid_label=True,padding=5,x_grid=True,y_grid=True, \
-                        xmin=-0,xmax=100,ymin=-1,size_hint=(.5,.5),pos=(200,275))
+                x_ticks_major=25,y_ticks_major=1,y_grid_label=True, \
+                x_grid_label=True,padding=5,x_grid=True,y_grid=True, \
+                xmin=-0,xmax=100,ymin=-1,size_hint=(.5,.5),pos=(200,275))
         plot = MeshLinePlot(color=[1,0,0,1])
         plot.points = [(x,700/x) for x in range(1,101)]
         graph.add_plot(plot)
-        label_layout.add_widget(graph)
-
-
-
-
-
-
-
 
         layout.bind(minimum_height=layout.setter('height'))
 
@@ -64,14 +66,13 @@ class Example(Screen):
 
         root = ScrollView(size_hint=(1, .4))
 
-
-
-
-
         root.add_widget(layout)
         root.bar_width = 10
         label_layout.add_widget(root)
         self.add_widget(label_layout)
+
+    def goBack(self,obj):
+            App.get_running_app().go_back()
 
 #if __name__ == '__main__':
  #   Example().run()
